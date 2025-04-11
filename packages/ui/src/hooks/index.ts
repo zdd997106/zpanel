033e05ef9a85a66a29b297f7c3483a3b8c6de5b0
Zdd Zhong <zz997106@gmail.com>
2025-04-11 15:58:42 +0800

    chore: Remove unuse functions

--- packages/ui/src/hooks/index.ts
+++ packages/ui/src/hooks/index.ts
@@ -3,5 +3,4 @@ export * from './useOpenList';
 export * from './useFallback';
 export * from './useRefresh';
 export * from './useGeneralErrorHandler';
-export * from './useGeneralAction';
 export * from './useSnackbar';
